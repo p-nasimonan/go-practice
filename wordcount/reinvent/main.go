bdb663bf6e673f5c51d71e4d9a155ed142f11074
p-nasimonan <pnasimonan@gmail.com>
2026-06-11 10:34:10 +0900

    自前でテスト関数作ったり、map比較したりできた

--- wordcount/reinvent/main.go
+++ wordcount/reinvent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func splitwords(text string) []string {
@@ -26,17 +27,47 @@ func splitwords(text string) []string {
 	return words
 }
 
-func main() {
-	text := "I am youkan I speak Japanese I have apple youkan is girls " //english only
+func WordCount(s string) map[string]int {
 
-	words := splitwords(text)
+	words := splitwords(s)
 
 	wordcount := make(map[string]int)
 	for _, word := range words {
 		wordcount[word]++
 	}
 
-	for word, count := range wordcount {
-		fmt.Printf("%s : %d 回\n", word, count)
+	return wordcount
+}
+
+func compareMaps(a, b map[string]int) bool {
+	if len(a) != len(b) {
+		return false
 	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func WordCountTest(wc func(s string) map[string]int) {
+	text := "I am apple I am Pen youkan is kawaii pupulie is kawaii"
+	wordcount := wc(text)
+
+	words := strings.Fields(text)
+
+	trustwc := make(map[string]int)
+	for _, word := range words {
+		trustwc[word]++
+	}
+	if compareMaps(trustwc, wordcount) {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
+}
+
+func main() {
+	WordCountTest(WordCount)
 }
